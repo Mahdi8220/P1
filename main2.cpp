cfa09ead1c5cebe1da369a7480ae84c394e26a06
Mahdi8220 <152307579+Mahdi8220@users.noreply.github.com>
2023-11-29 22:42:09 +0330

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+int x,y,z,f;
+cout<<"enter x value  :  ";
+  cin>>x;
+cout<<"inter y value  :  ";
+  cin>>y;
+cout<<"enter z value  :  ";
+  cin>>z;
+  f=(x-y)*(x+z)/(x+y+z)*(x+y+z)+1;
+
+cout <<f;
+	}
+	
