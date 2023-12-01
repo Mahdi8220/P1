a7738129c1d0228efafafc69c43a5f8416a525fa
Mahdi8220 <152307579+Mahdi8220@users.noreply.github.com>
2023-12-01 14:10:38 +0330

    Add files via upload

---
+++ hw5-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+char c;
+cout<<"please enter a charactor : ";
+cin>>c;
+switch(c){
+	case 'A':
+		cout<<"VOWEL";
+		break;
+	case 'a':
+		cout<<"VOWEL";
+		break;
+	case 'E':
+		cout<<"VOWEL";
+		break;
+	case 'e':
+		cout<<"VOWEL";
+		break;
+	case 'U':
+		cout<<"VOWEL";
+		break;
+	case 'u':
+		cout<<"VOWEL";
+		break;
+	case 'O':
+		cout<<"VOWEL";
+		break;
+	case 'o':
+		cout<<"VOWEL";
+		break;
+	case 'I':
+		cout<<"VOWEL";
+		break;
+	case 'i':
+		cout<<"VOWEL";
+  }
+}
+	
